5733ce3e8974ca6286c968ee2e991c8d18145b05
Cüneyt Hizli <cueneyt.hizli@fokus.fraunhofer.de>
2016-11-09 13:08:16 +0100

    automatic selection of filters is working

--- apps/datasetmanager/management/commands/eurostat.py
+++ apps/datasetmanager/management/commands/eurostat.py
@@ -1,151 +1,171 @@
-from django.core.management.base import BaseCommand, CommandError
-from apps.datasetmanager.models import Dataset
-from django.db import models
+from django.core.management.base import BaseCommand
 from django.apps import apps as django_apps
 from collections import OrderedDict
 from django.conf import settings
+from pandasdmx import Request
 import json
 import requests
 import datetime
-import pandas
+
 
 class Command(BaseCommand):
     help = 'Eurostat'
 
     def handle(self, *args, **options):
+        if len(args) == 1 and args[0] == 'ALL':
+            estat = Request("ESTAT")
+            dataflow = estat.get(resource_type='dataflow')
+            dataflows = dataflow.msg.dataflows
+            args = dataflows.keys()
+
+        filters = []
+        query = " "
+        filters_description = ""
+
         for code in args:
             data = requests.get("http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/" + str(code) + "?precision=1")
 
-            if data.status_code == 400:
-                errorDict = {"result": 400}
-                errorString = str(errorDict).replace("'", '"')
-                errorJson = json.loads(errorString)
-                return Response(errorJson)
-            elif data.status_code == 416:
-                errorDict = {"result": 416}
-                errorString = str(errorDict).replace("'", '"')
-                errorJson = json.loads(errorString)
-                return Response(errorJson)
+            if data.status_code == 416:
+                estat = Request("ESTAT")
+
+                dataflow = estat.get(resource_type='dataflow')
+
+                dataflows = dataflow.msg.dataflows
+
+                dsd_id = dataflows[code].structure.id
+
+                dsd_resp = estat.get(resource_type='datastructure', resource_id=dsd_id)
+
+                dsd = dsd_resp.msg.datastructures[dsd_id]
+
+                dimensionsList = list(dsd.dimensions)
+
+                for f in range(0, len(dimensionsList)):
+                    if(dimensionsList[f] != "TIME_PERIOD" and dimensionsList[f] != "GEO" and dimensionsList[f] != "FREQ"):
+                        dimensionsValuesList = list(dsd.dimensions[dimensionsList[f]].local_repr.enum.values())
+                        filters.append([dimensionsList[f], [dimensionsValuesList[0].id]])
+                        filters_description += dimensionsList[f] + ": " + dimensionsValuesList[0].name.en + ", "
+
+                if(filters):
+                    for key in range(0, len(filters)):
+                        for value in range(0, len(filters[key][1])):
+                            query += '&' + filters[key][0].lower() + '=' + filters[key][1][value]
+
+                data = requests.get("http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/" + str(code) + "?precision=1" + query.strip())
 
             array = data.json(object_pairs_hook=OrderedDict)
 
-            rowHeaders = array['dimension']['geo']['category']['label']
-
-            rowHeadersValues = list(rowHeaders.values())
-
-            rowLen = len(rowHeadersValues)
-
-            colHeaders = array['dimension']['time']['category']['label']
-
-            colHeadersVals = list(colHeaders.values())
-
-            colLen = len(colHeadersVals) + 1
-
-            colHeadersValues = []
-
-            colHeadersValues.append("")
-
-            for q in range(0, colLen - 1):
-                colHeadersValues.append(colHeadersVals[q])
-
-            val = array['value']
-
-            values = list(val.values())
-
-            rowArrays = []
-
-            index = 0
-            for row in range(0, rowLen):
-                colArray = []
-                position = row * (colLen - 1)
-                colArray.append(rowHeadersValues[row])
-                for col in range(0, colLen - 1):
-                    if val.get(str(position + col)) is None:
-                        colArray.append("")
-                    else:
-                        colArray.append(values[index])
-                        index += 1
-
-                rowArrays.append(colArray)
-
-            Individual = django_apps.get_model("referencepool", "Individual")
-            individuals_list = Individual.objects.all()
-
-            DataClass = django_apps.get_model("referencepool", "DataClass")
-            data_class = DataClass.objects.get(title="Country")
-
-            Indicator = django_apps.get_model("indicatorservice", "Indicator")
-
             try:
-                new_indicator = Indicator.objects.get(name=array['label'])
+                rowHeaders = array['dimension']['geo']['category']['label']
+
+                rowHeadersValues = list(rowHeaders.values())
+
+                rowLen = len(rowHeadersValues)
+
+                colHeaders = array['dimension']['time']['category']['label']
+
+                colHeadersVals = list(colHeaders.values())
+
+                colLen = len(colHeadersVals) + 1
+
+                colHeadersValues = []
+
+                colHeadersValues.append("")
+
+                for q in range(0, colLen - 1):
+                    colHeadersValues.append(colHeadersVals[q])
+
+                val = array['value']
+
+                values = list(val.values())
+
+                rowArrays = []
+
+                index = 0
+                for row in range(0, rowLen):
+                    colArray = []
+                    position = row * (colLen - 1)
+                    colArray.append(rowHeadersValues[row])
+                    for col in range(0, colLen - 1):
+                        if val.get(str(position + col)) is None:
+                            colArray.append("")
+                        else:
+                            colArray.append(values[index])
+                            index += 1
+
+                    rowArrays.append(colArray)
+
+                Individual = django_apps.get_model("referencepool", "Individual")
+                individuals_list = Individual.objects.all()
+
+                DataClass = django_apps.get_model("referencepool", "DataClass")
+                data_class = DataClass.objects.get(title="Country")
+
+                Indicator = django_apps.get_model("indicatorservice", "Indicator")
+
+                try:
+                    new_indicator = Indicator.objects.get(name=array['label'])
+                except:
+                    new_indicator = Indicator(name=array['label'], description=array['label'])
+                    new_indicator.save()
+
+                spatials = []
+
+                for i in range(0, len(rowHeaders)):
+                    spatial_key = list(rowHeaders.keys())[i]
+                    spatial_value = list(rowHeaders.values())[i]
+                    found = False
+                    for individual in individuals_list:
+                        if spatial_value == individual.title:
+                            spatials.append(individual.id)
+                            found = True
+
+                    if found is False:
+                        new_individual = Individual(title=spatial_value, code=spatial_key, data_class=data_class)
+                        new_individual.save()
+                        spatials.append(new_individual.id)
+
+                resultArray = []
+
+                for row in range(0, len(rowArrays)):
+                    values = {}
+                    spatial = ""
+                    for individual in individuals_list:
+                        if rowArrays[row][0] == individual.title:
+                            spatial = individual.title
+
+                    for i in range(1, len(colHeadersValues)):
+                        if type(rowArrays[row][i]) is str:
+                            values[colHeadersValues[i]] = None
+                        else:
+                            values[colHeadersValues[i]] = rowArrays[row][i]
+
+                    resultArray.append({"row": row + 1, "individual": spatial, "values": values})
+
+                headers = {'content-type': 'application/json', 'x-user-path': 'https://adhocracy-prod.policycompass.eu/api/principals/users/0000072/', 'x-user-token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE0Nzg1Mjg2NDIsInN1YiI6Ii9wcmluY2lwYWxzL3VzZXJzLzAwMDAwNzIiLCJleHAiOjE0ODExMjA2NDJ9.HhV3dnlZp_B_A0q6Ijq7MMwC8tbcDMz72ZqP2XW9WCumLZVVoMoGSaxk7CyZkC-F4jSHrx4htn9h5ZqOx35l3Q'}
+
+                payload = {}
+                payload['time'] = {"resolution": "year", "start": colHeadersValues[1], "end": colHeadersValues[len(colHeadersValues) - 1]}
+                payload["resource"] = {"issued": datetime.datetime.today().strftime('%Y-%m-%d'), "external_resource": 71, "url": "http://appsso.eurostat.ec.europa.eu/nui/show.do?dataset=" + str(code) + "&lang=en"}
+                payload["policy_domains"] = [1]
+                payload["title"] = array['label']
+                payload["keywords"] = array['label']
+                payload["license_id"] = 96
+                payload["description"] = array['label']
+                payload["spatials"] = spatials
+                payload["language_id"] = 38
+                payload["indicator_id"] = new_indicator.id
+                payload["class_id"] = 1
+                payload["user_id"] = 1
+                payload["unit_id"] = 25
+                payload["data"] = {"table": resultArray}
+                payload["is_draft"] = False
+
+                if len(filters) > 0:
+                    payload['description'] = payload['description'] + " (selected filters: " + filters_description[:-2] + ")"
+
+                requests.post(settings.PC_SERVICES['references']['base_url'] + '/api/v1/datasetmanager/datasets', data=json.dumps(payload), headers=headers)
+                print("Download of '", code, "' successful")
+
             except:
-                new_indicator = Indicator(name=array['label'], description=array['label'])
-                new_indicator.save()
-
-
-            spatials = []
-
-
-            for i in range(0, len(rowHeaders)):
-                spatial_key = list(rowHeaders.keys())[i]
-                spatial_value = list(rowHeaders.values())[i]
-                found = False
-                for individual in individuals_list:
-                    if spatial_value == individual.title:
-                        spatials.append(individual.id)
-                        found = True
-
-                if found is False:
-                    new_individual = Individual(title=spatial_value, code=spatial_key, data_class=data_class)
-                    new_individual.save()
-                    spatials.append(new_individual.id)
-
-
-            resultArray = []
-
-            for row in range(0, len(rowArrays)):
-                values = {}
-                spatial = ""
-                for individual in individuals_list:
-                    if rowArrays[row][0] == individual.title:
-                        spatial = individual.title
-
-                for i in range(1, len(colHeadersValues)):
-                    if type(rowArrays[row][i]) is str:
-                        values[colHeadersValues[i]] = None
-                    else:
-                        values[colHeadersValues[i]] = rowArrays[row][i]
-
-
-                resultArray.append({"row":row+1, "individual":spatial, "values":values})
-
-
-            headers = {'content-type': 'application/json', 'x-user-path':'https://adhocracy-prod.policycompass.eu/api/principals/users/0000072/',
-                       'x-user-token':'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE0Nzg1Mjg2NDIsInN1YiI6Ii9wcmluY2lwYWxzL3VzZXJzLzAwMDAwNzIiLCJleHAiOjE0ODExMjA2NDJ9.HhV3dnlZp_B_A0q6Ijq7MMwC8tbcDMz72ZqP2XW9WCumLZVVoMoGSaxk7CyZkC-F4jSHrx4htn9h5ZqOx35l3Q'}
-
-            payload = {"time":
-                    {"resolution":"year","start":colHeadersValues[1],"end":colHeadersValues[len(colHeadersValues)-1]},
-                    "resource":{"issued":datetime.datetime.today().strftime('%Y-%m-%d'),
-                        "external_resource":71,
-                        "url":"http://appsso.eurostat.ec.europa.eu/nui/show.do?dataset="+ str(code) +"&lang=en"
-                    },
-                    "policy_domains":[1],
-                    "title":array['label'],
-                    "keywords":array['label'],
-                    "license_id":96,
-                    "description":array['label'],
-                    "spatials":spatials,
-                    "language_id":38,
-                    "indicator_id":new_indicator.id,
-                    "class_id":1,
-                    "user_id":1,
-                    "unit_id":25,
-                    "data":{"table":resultArray},
-                    "is_draft":True}
-
-
-
-            print("payload " , json.dumps({"table":resultArray}))
-
-            result = requests.post(settings.PC_SERVICES['references']['base_url'] + '/api/v1/datasetmanager/datasets', data=json.dumps(payload), headers=headers)
-
+                print("ERROR: Download of '", code, "' unsuccessful")
