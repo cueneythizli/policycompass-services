4e5ce6e66421aba9f1b5bdca7731bfca989eabfb
Cüneyt Hizli <cueneyt.hizli@fokus.fraunhofer.de>
2016-11-09 13:31:00 +0100

    added comments

--- apps/datasetmanager/management/commands/eurostat.py
+++ apps/datasetmanager/management/commands/eurostat.py
@@ -12,7 +12,9 @@ class Command(BaseCommand):
     help = 'Eurostat'
 
     def handle(self, *args, **options):
+        # if all datasets should be downloaded
         if len(args) == 1 and args[0] == 'ALL':
+            # usage of pandasdmx
             estat = Request("ESTAT")
             dataflow = estat.get(resource_type='dataflow')
             dataflows = dataflow.msg.dataflows
@@ -22,24 +24,22 @@ class Command(BaseCommand):
         query = " "
         filters_description = ""
 
+        # iterate over every Eurostat code in arguments
         for code in args:
             data = requests.get("http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/" + str(code) + "?precision=1")
 
+            # selection of filters
             if data.status_code == 416:
+                # usage of pandasdmx
                 estat = Request("ESTAT")
-
                 dataflow = estat.get(resource_type='dataflow')
-
                 dataflows = dataflow.msg.dataflows
-
                 dsd_id = dataflows[code].structure.id
-
                 dsd_resp = estat.get(resource_type='datastructure', resource_id=dsd_id)
-
                 dsd = dsd_resp.msg.datastructures[dsd_id]
-
                 dimensionsList = list(dsd.dimensions)
 
+                # choose first option of any filter, except TIME_PERIOD, GEO and FREQ
                 for f in range(0, len(dimensionsList)):
                     if(dimensionsList[f] != "TIME_PERIOD" and dimensionsList[f] != "GEO" and dimensionsList[f] != "FREQ"):
                         dimensionsValuesList = list(dsd.dimensions[dimensionsList[f]].local_repr.enum.values())
@@ -51,37 +51,31 @@ class Command(BaseCommand):
                         for value in range(0, len(filters[key][1])):
                             query += '&' + filters[key][0].lower() + '=' + filters[key][1][value]
 
+                # new request with selected filters
                 data = requests.get("http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/" + str(code) + "?precision=1" + query.strip())
 
             array = data.json(object_pairs_hook=OrderedDict)
 
             try:
+                # get countries
                 rowHeaders = array['dimension']['geo']['category']['label']
-
                 rowHeadersValues = list(rowHeaders.values())
-
                 rowLen = len(rowHeadersValues)
-
+                # get years
                 colHeaders = array['dimension']['time']['category']['label']
-
                 colHeadersVals = list(colHeaders.values())
-
                 colLen = len(colHeadersVals) + 1
-
                 colHeadersValues = []
-
                 colHeadersValues.append("")
 
                 for q in range(0, colLen - 1):
                     colHeadersValues.append(colHeadersVals[q])
 
                 val = array['value']
-
                 values = list(val.values())
-
                 rowArrays = []
-
                 index = 0
+
                 for row in range(0, rowLen):
                     colArray = []
                     position = row * (colLen - 1)
@@ -111,6 +105,7 @@ class Command(BaseCommand):
 
                 spatials = []
 
+                # choose already existing individuals from referencepool or create missing individuals
                 for i in range(0, len(rowHeaders)):
                     spatial_key = list(rowHeaders.keys())[i]
                     spatial_value = list(rowHeaders.values())[i]
@@ -127,6 +122,7 @@ class Command(BaseCommand):
 
                 resultArray = []
 
+                # create result array in correct format
                 for row in range(0, len(rowArrays)):
                     values = {}
                     spatial = ""
@@ -142,8 +138,10 @@ class Command(BaseCommand):
 
                     resultArray.append({"row": row + 1, "individual": spatial, "values": values})
 
+                # headers for POST request
                 headers = {'content-type': 'application/json', 'x-user-path': 'https://adhocracy-prod.policycompass.eu/api/principals/users/0000072/', 'x-user-token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE0Nzg1Mjg2NDIsInN1YiI6Ii9wcmluY2lwYWxzL3VzZXJzLzAwMDAwNzIiLCJleHAiOjE0ODExMjA2NDJ9.HhV3dnlZp_B_A0q6Ijq7MMwC8tbcDMz72ZqP2XW9WCumLZVVoMoGSaxk7CyZkC-F4jSHrx4htn9h5ZqOx35l3Q'}
 
+                # prepare new dataset
                 payload = {}
                 payload['time'] = {"resolution": "year", "start": colHeadersValues[1], "end": colHeadersValues[len(colHeadersValues) - 1]}
                 payload["resource"] = {"issued": datetime.datetime.today().strftime('%Y-%m-%d'), "external_resource": 71, "url": "http://appsso.eurostat.ec.europa.eu/nui/show.do?dataset=" + str(code) + "&lang=en"}
@@ -161,6 +159,7 @@ class Command(BaseCommand):
                 payload["data"] = {"table": resultArray}
                 payload["is_draft"] = False
 
+                # add selected filters to description
                 if len(filters) > 0:
                     payload['description'] = payload['description'] + " (selected filters: " + filters_description[:-2] + ")"
 
